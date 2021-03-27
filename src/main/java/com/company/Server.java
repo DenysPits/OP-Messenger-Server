6ab2d3c43de7f97c4934b0772a7ef8b3244cba29
Denys Pits <lykoshko4@gmail.com>
2021-03-27 21:45:38 +0200

    add server data from file

--- src/main/java/com/company/Server.java
+++ src/main/java/com/company/Server.java
@@ -4,14 +4,28 @@ import com.company.handlers.MessageHandler;
 import com.company.handlers.UserHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.sql.*;
 
 public class Server {
-    public static final String url = "jdbc:mysql://localhost:3306/messenger_database";
+    public static String url; //"jdbc:mysql://localhost:3306/messenger_database";
     public static final String username = "user";
     public static final String password = "root";
+    public static String address;
+
+    public static void readServerData() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            url = reader.readLine();
+            address = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
     public static void main(String[] args) throws IOException {
         try {
@@ -22,12 +36,14 @@ public class Server {
         }
 
         try {
+            readServerData();
             Connection connection = DriverManager.getConnection(url, username, password);
-            HttpServer server = HttpServer.create();
-            server.bind(new InetSocketAddress(8000), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(address, 8001), 0);
+            //HttpServer server = HttpServer.create();
+            //server.bind(new InetSocketAddress(, 8000), 0);
             System.out.println("Server was bind");
-            server.createContext("/api/messages/", new MessageHandler(connection));
-            server.createContext("/api/users/", new UserHandler(connection));
+            server.createContext("/api/messages", new MessageHandler(connection));
+            server.createContext("/api/users", new UserHandler(connection));
             server.setExecutor(null);
             server.start();
         } catch (SQLException throwables) {
